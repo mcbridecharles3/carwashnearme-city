e2ede10385dcce0b377d09c371bd7f6c9428d9a6
mcbridecharles3 <mcbridecharles3@gmail.com>
2025-06-20 23:04:46 -0500

    big pub

---
+++ massachusetts/js/rev-custom.js
@@ -0,0 +1,189 @@
+// page-1 title
+var tpj = jQuery;
+if (window.RS_MODULES === undefined) window.RS_MODULES = {};
+if (RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+RS_MODULES.modules["revslider11"] = {
+    once: RS_MODULES.modules["revslider11"] !== undefined ? RS_MODULES.modules["revslider11"].once : undefined, init: function () {
+        window.revapi1 = window.revapi1 === undefined || window.revapi1 === null || window.revapi1.length === 0 ? document.getElementById("rev_slider_1_1") : window.revapi1;
+        if (window.revapi1 === null || window.revapi1 === undefined || window.revapi1.length == 0) { window.revapi1initTry = window.revapi1initTry === undefined ? 0 : window.revapi1initTry + 1; if (window.revapi1initTry < 20) requestAnimationFrame(function () { RS_MODULES.modules["revslider11"].init() }); return; }
+        window.revapi1 = jQuery(window.revapi1);
+        if (window.revapi1.revolution == undefined) { revslider_showDoubleJqueryError("rev_slider_1_1"); return; }
+        revapi1.revolutionInit({
+            revapi: "revapi1",
+            DPR: "dpr",
+            sliderLayout: "fullscreen",
+            visibilityLevels: "1240,1024,778,480",
+            gridwidth: "1650,1024,778,480",
+            gridheight: "950,650,500,350",
+            lazyType: "smart",
+            perspective: 600,
+            perspectiveType: "global",
+            editorheight: "950,650,500,350",
+            responsiveLevels: "1240,1024,778,480",
+            progressBar: { disableProgressBar: true },
+            navigation: {
+                wheelCallDelay: 1000,
+                onHoverStop: false,
+                arrows: {
+                    enable: true,
+                    tmp: "<div class=\"tp-title-wrap\">  	<div class=\"tp-arr-imgholder\"></div> </div>",
+                    style: "zeus",
+                    hide_onmobile: true,
+                    hide_under: "1399px",
+                    hide_onleave: true,
+                    animSpeed: "100ms",
+                    animDelay: "100ms",
+                    left: {
+                        h_offset: 30
+                    },
+                    right: {
+                        h_offset: 30
+                    }
+                },
+                bullets: {
+                    enable: true,
+                    tmp: "",
+                    style: "hesperiden",
+                    v_offset: 30
+                }
+            },
+            viewPort: {
+                global: true,
+                globalDist: "-200px",
+                enable: false
+            },
+            fallbacks: {
+                allowHTML5AutoPlayOnAndroid: true
+            },
+        });
+
+    }
+} // End of RevInitScript
+if (window.RS_MODULES.checkMinimal !== undefined) { window.RS_MODULES.checkMinimal(); };
+
+// page-2 title
+var	tpj = jQuery;
+					if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+					if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+					RS_MODULES.modules["revslider31"] = {once: RS_MODULES.modules["revslider31"]!==undefined ? RS_MODULES.modules["revslider31"].once : undefined, init:function() {
+						window.revapi3 = window.revapi3===undefined || window.revapi3===null || window.revapi3.length===0  ? document.getElementById("rev_slider_3_1") : window.revapi3;
+						if(window.revapi3 === null || window.revapi3 === undefined || window.revapi3.length==0) { window.revapi3initTry = window.revapi3initTry ===undefined ? 0 : window.revapi3initTry+1; if (window.revapi3initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider31"].init()}); return;}
+						window.revapi3 = jQuery(window.revapi3);
+						if(window.revapi3.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_3_1"); return;}
+						revapi3.revolutionInit({
+								revapi:"revapi3",
+								DPR:"dpr",
+								sliderLayout:"fullwidth",
+								visibilityLevels:"1240,1024,778,480",
+								gridwidth:"1650,1024,778,480",
+								gridheight:"950,650,500,690",
+								lazyType:"smart",
+								perspective:600,
+								perspectiveType:"global",
+								editorheight:"950,650,500,690",
+								responsiveLevels:"1240,1024,778,480",
+								progressBar:{disableProgressBar:true},
+								navigation: {
+									wheelCallDelay:1000,
+									onHoverStop:false,
+									arrows: {
+										enable:true,
+										tmp:"<div class=\"tp-title-wrap\">  	<div class=\"tp-arr-imgholder\"></div> </div>",
+										style:"zeus",
+										hide_onmobile:true,
+										hide_under:"1399px",
+										hide_onleave:true,
+										animSpeed:"100ms",
+										animDelay:"100ms",
+										left: {
+											h_offset:30
+										},
+										right: {
+											h_offset:30
+										}
+									},
+									bullets: {
+										enable:true,
+										tmp:"",
+										style:"hesperiden",
+										hide_onmobile:true,
+										hide_under:"479px",
+										v_offset:30
+									}
+								},
+								viewPort: {
+									global:true,
+									globalDist:"-200px",
+									enable:false
+								},
+								fallbacks: {
+									allowHTML5AutoPlayOnAndroid:true
+								},
+						});
+						
+					}} // End of RevInitScript
+					if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
+
+
+					// page-3 title
+					var	tpj = jQuery;
+					if(window.RS_MODULES === undefined) window.RS_MODULES = {};
+					if(RS_MODULES.modules === undefined) RS_MODULES.modules = {};
+					RS_MODULES.modules["revslider71"] = {once: RS_MODULES.modules["revslider71"]!==undefined ? RS_MODULES.modules["revslider71"].once : undefined, init:function() {
+						window.revapi7 = window.revapi7===undefined || window.revapi7===null || window.revapi7.length===0  ? document.getElementById("rev_slider_7_1") : window.revapi7;
+						if(window.revapi7 === null || window.revapi7 === undefined || window.revapi7.length==0) { window.revapi7initTry = window.revapi7initTry ===undefined ? 0 : window.revapi7initTry+1; if (window.revapi7initTry<20) requestAnimationFrame(function() {RS_MODULES.modules["revslider71"].init()}); return;}
+						window.revapi7 = jQuery(window.revapi7);
+						if(window.revapi7.revolution==undefined){ revslider_showDoubleJqueryError("rev_slider_7_1"); return;}
+						revapi7.revolutionInit({
+								revapi:"revapi7",
+								DPR:"dpr",
+								sliderLayout:"fullwidth",
+								visibilityLevels:"1240,1024,778,480",
+								gridwidth:"1650,1024,778,480",
+								gridheight:"1230,650,630,440",
+								lazyType:"smart",
+								perspective:600,
+								perspectiveType:"global",
+								editorheight:"1230,650,630,440",
+								responsiveLevels:"1240,1024,778,480",
+								progressBar:{disableProgressBar:true},
+								navigation: {
+									wheelCallDelay:1000,
+									onHoverStop:false,
+									arrows: {
+										enable:true,
+										tmp:"<div class=\"tp-title-wrap\">  	<div class=\"tp-arr-imgholder\"></div> </div>",
+										style:"zeus",
+										hide_onmobile:true,
+										hide_under:"1399px",
+										hide_onleave:true,
+										animSpeed:"100ms",
+										animDelay:"100ms",
+										left: {
+											h_offset:30,
+											v_offset:220
+										},
+										right: {
+											h_offset:30,
+											v_offset:220
+										}
+									},
+									bullets: {
+										enable:true,
+										tmp:"",
+										style:"hesperiden",
+										v_offset:30
+									}
+								},
+								viewPort: {
+									global:true,
+									globalDist:"-200px",
+									enable:false
+								},
+								fallbacks: {
+									allowHTML5AutoPlayOnAndroid:true
+								},
+						});
+						
+					}} // End of RevInitScript
+					if (window.RS_MODULES.checkMinimal!==undefined) { window.RS_MODULES.checkMinimal();};
